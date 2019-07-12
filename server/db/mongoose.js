aa4f67e9049cb0c2c90dcb62a4075eee09e1f901
unknown <harshilmodi562000@yahoo.com>
2019-07-12 17:55:51 +0530

    Local anmongoose.connect(`mongodb://localhost:27017/TodoApp`, { useNewUrlParser: true });

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect(`mongodb://localhost:27017/TodoApp`, { useNewUrlParser: true });
-mongoose.connect(`mongodb+srv://admin:Admin@123@node-course-nl65m.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true }).then(() => {
-    console.log("Connected to mongodb.net");
-});
+
+if (process.env.PORT) {
+    mongoose.connect(`mongodb+srv://admin:Admin@123@node-course-nl65m.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true }).then(() => {
+        console.log("Connected to mongodb.net");
+    });
+} else {
+    mongoose.connect(`mongodb://localhost:27017/TodoApp`, { useNewUrlParser: true });
+    
+}
 
 module.exports = { mongoose };
