b1107e6bfd58bab9c12ed7c3272640f869d21e2a
unknown <harshilmodi562000@yahoo.com>
2019-07-12 17:15:48 +0530

    MongoLab connect

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -6,31 +6,4 @@ var { Todo } = require('./../server/models/todo');
 var { User } = require('./../server/models/user');
 
 
-var id = '5d03b78282216b1bd4939bc1';
-
-// if (!ObjectId.isValid(id)) {
-//     console.log("id not valid");
-// } else {
-    
-// }
-
-// Todo.find({
-//     _id: id
-// }).then((docs) => {
-//     console.log("find()\n", JSON.stringify(docs, undefined , 2));
-// }, (e) => console.log(e));
-
-// Todo.findOne({
-//     _id: id
-// }).then((doc) => {
-//     console.log("\n\nfindOne()\n", JSON.stringify(doc, undefined, 2));
-// }, (e) => console.log(e));
-
-// Todo.findById(id).then((doc) => {
-//     console.log("\n\nfindById()\n", JSON.stringify(doc, undefined, 2));
-// }, (e) => console.log(e));
-
-User.findById(id).then((doc) => {
-    // debugger;
-    console.log(doc);
-}, (e) => console.log(e));
+db.mynewcollection.insert({ "foo": "bar" });
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://localhost:27017/TodoApp`, { useNewUrlParser: true });
-
+// mongoose.connect(`mongodb://localhost:27017/TodoApp`, { useNewUrlParser: true });
+mongoose.connect(`mongodb+srv://admin:Admin@123@node-course-nl65m.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true }).then(() => {
+    console.log("Connected to mongodb.net");
+});
 
 module.exports = { mongoose };
